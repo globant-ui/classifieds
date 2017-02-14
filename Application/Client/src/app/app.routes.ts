84b77f696ebb987d94d386a6b55dff853a29b700
lakuleavadhutglobant <satish.shewale@globant.com>
2017-02-14 13:01:16 +0530

    router link and login localhost

--- Application/Client/src/app/app.routes.ts
+++ Application/Client/src/app/app.routes.ts
@@ -7,6 +7,9 @@ import { LoginComponent } from './_common/login/component/login.component';
 
 export const ROUTES: Routes = [
   { path: '', component: LoginComponent},
-  { path: 'home', component: HomeComponent},
-  { path: 'product-info', component: ProductInfoComponent}
+  {
+    path: 'home', component: HomeComponent
+  },
+  {path: 'productInfo', component: ProductInfoComponent}
+
 ];
