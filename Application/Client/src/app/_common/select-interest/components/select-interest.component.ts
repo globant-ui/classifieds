b0e4706a8d2fe51f850bc9c9fee05058872a80d9
Sagar Mahindrakar <sagar.mahindrakar@globant.com>
2017-02-27 16:02:29 +0530

    changes done

--- Application/Client/src/app/_common/select-interest/components/select-interest.component.ts
+++ Application/Client/src/app/_common/select-interest/components/select-interest.component.ts
@@ -24,8 +24,8 @@ let tpls = require('../tpls/select-interest.component.html').toString();
 export class SelectInterestComponent implements OnInit, AfterViewInit  {
 
 private delayTimer  : any = null;
-private subCategoryUrl:string = "";
-private selectInterestUrl:string = "";
+private subCategoryUrl:string = '';
+private selectInterestUrl:string = '';
 private interestResult:any ;
 private obj = {
   'SubCategory': [],
@@ -46,16 +46,16 @@ private session : Session;
                         this.selectInterestUrl = _settingsService.getPath('searchUrl');
                  }
 
-  ngOnInit(){
+  ngOnInit() {
       this.emailId = this._cookieService.getObject('SESSION_PORTAL')["useremail"];
-    }
+  }
 
-    ngAfterViewInit(){
-        this.session = new Session(this._cookieService.getObject('SESSION_PORTAL'));
-        if (this.session['isFirstTimeLogin']) {
-            $("#myModal").modal("show");
-        }
-    } 
+  ngAfterViewInit() {
+      this.session = new Session(this._cookieService.getObject('SESSION_PORTAL'));
+      if (this.session['isFirstTimeLogin']==true) {
+          $("#myModal").modal("show");
+      }
+  } 
 
   fetchInterest(e: Event, val) {
       if (val.length >= 3) {
@@ -68,83 +68,81 @@ private session : Session;
       }
   }
       //get call to get the dropdowndown list
-    fetchInterestData(text: string) {
-        this._cservice.observableGetHttp(this.subCategoryUrl + text, null, false)
-            .subscribe((res: Response) => {
-                if (res['length'] > 0) {
-                    this.interestResult = res;
-                    this.enabledDropdown = true;
-                } else {
-                    console.log("No result found!!!");
-                }
-            },
-            error => {
-                console.log("error in response",error);
-            });
-    }
+  fetchInterestData(text: string) {
+      this._cservice.observableGetHttp(this.subCategoryUrl + text, null, false)
+          .subscribe((res: Response) => {
+              if (res['length'] > 0) {
+                  this.interestResult = res;
+                  this.enabledDropdown = true;
+              } else {
+                  console.log("No result found!!!");
+              }
+          },
+          error => {
+              console.log("error in response", error);
+          });
+  }
 
 //to select an item from dropdown
-    selectInterest(val) {
-        this.obj.SubCategory.push(val);
-        //to duplicate element allowed in tag logic
-        this.obj.SubCategory = this.obj.SubCategory.reduce(function (a, b) {
-            if (a.indexOf(b) < 0) a.push(b);
-            return a;
-        }, []);
-        this.enabledDropdown = false;
-        this.isValid = false;
-        this.interestResult = [];
-    }
-     
+  selectInterest(val) {
+      this.obj.SubCategory.push(val);
+      //to duplicate element allowed in tag logic
+      this.obj.SubCategory = this.obj.SubCategory.reduce(function (a, b) {
+          if (a.indexOf(b) < 0) a.push(b);
+          return a;
+      }, []);
+      this.enabledDropdown = false;
+      this.isValid = false;
+      this.interestResult = [];
+  }
+
     //to delete an item from tagList
-    deleteIntrest(index, val) {
-        let delFlag = this.obj.SubCategory.splice(index, 1);
-    }
+  deleteIntrest(index, val) {
+      let delFlag = this.obj.SubCategory.splice(index, 1);
+  }
 
     //to close the pop-up window
-    skipInterest(val){
-        $("#myModal").modal("hide");
-    }
+  skipInterest(val) {
+      $("#myModal").modal("hide");
+  }
 
     //checkbox code
-    selectCheckbox(element: HTMLInputElement): void {
-           
-        let elemIndex = this.obj.Location.indexOf(element.value);
-        
-        if( element.value != 'All' && element.checked ) {
-                this.obj.Location.push( element.value );
-                
-        }
-            else {
-                this.obj.Location.splice( elemIndex, 1 );   
-                this.disabledCheckbox = false;
-            }
-         if( element.value == 'All' ) {
-                this.obj.Location.length = 0;
+  selectCheckbox(element: HTMLInputElement): void {
 
-                if( element.checked ) {
-                    this.obj.Location.push( "Pune", "Banglore" ); 
-                    this.disabledCheckbox = true;
-                }
-                
-            }
-        } 
+      let elemIndex = this.obj.Location.indexOf(element.value);
+
+      if (element.value != 'All' && element.checked) {
+          this.obj.Location.push(element.value);
+
+      }
+      else {
+          this.obj.Location.splice(elemIndex, 1);
+          this.disabledCheckbox = false;
+      }
+      if (element.value == 'All') {
+          this.obj.Location.length = 0;
+
+          if (element.checked) {
+              this.obj.Location.push("Pune", "Banglore");
+              this.disabledCheckbox = true;
+          }
+      }
+  } 
 
     //post call
-    PostData(obj){
-        if(obj.Location.length === 0){
-            obj.Location.push("Pune","Banglore");
-         }
-          this._cservice.observablePostHttp(this.selectInterestUrl+this.emailId, obj, null, false)
-            .subscribe((res: Response) => {
-                console.log("postcall response",res)
-            },
-            error => {
-                console.log("error in response",error);
-            });
+  PostData(obj) {
+      if (obj.Location.length === 0) {
+          obj.Location.push("Pune", "Banglore");
+      }
+      this._cservice.observablePostHttp(this.selectInterestUrl + this.emailId, obj, null, false)
+          .subscribe((res: Response) => {
+              console.log("postcall response", res)
             $("#myModal").modal("hide");
-   
-    }
+          },
+          error => {
+              console.log("error in response", error);
+          });
+  }
 
 } 
 
