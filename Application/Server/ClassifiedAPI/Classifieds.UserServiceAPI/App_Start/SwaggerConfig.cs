d5ed73acc9af6e601484865d3ebac9238601a257
pawar-amol <amol.pawar@globant.com>
2017-03-02 18:13:18 +0530

    Integrated Swagger for online documentation

--- Application/Server/ClassifiedAPI/Classifieds.UserServiceAPI/App_Start/SwaggerConfig.cs
+++ Application/Server/ClassifiedAPI/Classifieds.UserServiceAPI/App_Start/SwaggerConfig.cs
@@ -97,7 +97,7 @@ namespace Classifieds.UserServiceAPI
                         // those comments into the generated docs and UI. You can enable this by providing the path to one or
                         // more Xml comment files.
                         //
-                        //c.IncludeXmlComments(GetXmlCommentsPath());
+                        c.IncludeXmlComments(GetXmlCommentsPath());
 
                         // Swashbuckle makes a best attempt at generating Swagger compliant JSON schemas for the various types
                         // exposed in your API. However, there may be occasions when more control of the output is needed.
@@ -197,7 +197,7 @@ namespace Classifieds.UserServiceAPI
                         // in a badge at the bottom of the page. Use these options to set a different validator URL or to disable the
                         // feature entirely.
                         //c.SetValidatorUrl("http://localhost/validator");
-                        //c.DisableValidator();
+                        c.DisableValidator();
 
                         // Use this option to control how the Operation listing is displayed.
                         // It can be set to "None" (default), "List" (shows operations for each resource),
@@ -241,6 +241,11 @@ namespace Classifieds.UserServiceAPI
                         //
                         //c.EnableApiKeySupport("apiKey", "header");
                     });
+        }
+
+        private static string GetXmlCommentsPath()
+        {
+            return string.Format(@"{0}\bin\Classifieds.UserServiceAPI.XML", System.AppDomain.CurrentDomain.BaseDirectory);
         }
     }
 }
