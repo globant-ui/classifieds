958b393ce36aa241ee3c60c4d8db493906cccd6d
Sagar Mahindrakar <sagar.mahindrakar@globant.com>
2017-03-13 16:53:56 +0530

    modified the post del api call

--- Application/Client/src/app/card-list/components/card-list.component.ts
+++ Application/Client/src/app/card-list/components/card-list.component.ts
@@ -24,37 +24,39 @@ export class CardListComponent{
     private emailId:string = '';
     private filterCategoryUrl:string = '';
     private GetUserWishList: string = '';
+    private DeleteUserWishListUrl: string = '';
+    private updatedCards = [];
+    private isLoading:boolean = false;
 
     @Input() cards;
 
     constructor(
-                public appState: AppState,
-                private _settingsService : SettingsService,
-                private _router:Router,
-                private _cservice:CService,
-                private _cookieService:CookieService
-                 ) {
-                    this.favoriteUrl = _settingsService.getPath('favoriteUrl');
-                    this.filterCategoryUrl = _settingsService.getPath('filterCategoryUrl');
-                    this.GetUserWishList = _settingsService.getPath('GetUserWishList');
-                 }
+        public appState: AppState,
+        private _settingsService: SettingsService,
+        private _router: Router,
+        private _cservice: CService,
+        private _cookieService: CookieService
+    ) {
+        this.favoriteUrl = _settingsService.getPath('favoriteUrl');
+        this.filterCategoryUrl = _settingsService.getPath('filterCategoryUrl');
+        this.GetUserWishList = _settingsService.getPath('GetUserWishList');
+        this.DeleteUserWishListUrl = _settingsService.getPath('DeleteUserWishListUrl');
+    }
+
     ngOnChanges(changes:SimpleChanges){
         if(changes['cards']){
             this.GetWishList();
         }
     }
 
-    private updatedCards = [];
-    private isLoading:boolean = false;
-    private listingId:number;
-    
     ngOnInit() {
-        console.log('all = ',this.cards);
+        console.log('all = ', this.cards);
         this.emailId = this._cookieService.getObject('SESSION_PORTAL')["useremail"];
-        console.log("card-list email",this.emailId)
+        console.log("card-list email", this.emailId);
+        this.GetUserWishList = this.GetUserWishList + this.emailId;
     }
-        
-    loading( flag ) {
+
+    loading(flag) {
         this.isLoading = flag;
     }
 
@@ -62,45 +64,70 @@ export class CardListComponent{
      this.showProductInfoPage = true;
      this._router.navigate(['productInfo',id]);
   }
-//favorite card method
-  favorite(id, i) {
-      debugger;
-     // this.favColor = !this.favColor;
-      //post call for favorite 
-      this.listingId = id; 
-      this.favoriteUrl = this.favoriteUrl + this.emailId + '&listingId='+id;
-        this._cservice.observablePostHttp(this.favoriteUrl , id, null, false)
-          .subscribe((res: Response) => {
-              this.updatedCards[i]['isInWishList'] = true;
-              console.log("postcall response", res);
-          },
-          error => {
-              console.log("error in response", error);
-          });
-  }
-  GetWishList(){
-        this._cservice.observableGetHttp(this.GetUserWishList + this.emailId, null, false)
-          .subscribe((res: Response) => {
-            if(res['length']!=0){
-                this.updateCards(res);
-            }
-          },
-          error => {
-              console.log("error in response", error);
-          });
-  }
-  updateCards(wishListData){
-      if( this.cards && this.cards.length != 0 ) {
-          for(let i=0;i<this.cards.length;i++) {
-            if( wishListData.indexOf(this.cards[i]['_id']) == -1 ){
-                this.cards[i]['isInWishList'] = false;
-            } else {
-                this.cards[i]['isInWishList'] = true;
-            }
 
-            this.updatedCards.push( this.cards[i] );
-        }
-        console.log( this.updatedCards );
-      } 
+//favorite card method
+  favorite(id, i,card) {
+     // if (card['isInWishList'] === undefined || card['isInWishList'] === false) {
+          console.log("card : ",card)
+          console.log("this.favoriteUrl",this.favoriteUrl)
+          this.favoriteUrl = (card.isInWishList ? this.DeleteUserWishListUrl : this.favoriteUrl) + ( this.emailId + '&listingId=' + id);
+          var operation = card.isInWishList ? 'observableDeleteHttp' :'observablePostHttp';
+          this._cservice[operation]( this.favoriteUrl,card.isInWishList ? null : card, card.isInWishList ? false: null, false)
+              .subscribe((res: Response) => {
+                //   this.updatedCards = this.cards;
+                  this.cards[i]['isInWishList'] = !this.cards[i]['isInWishList'];
+                //   console.log("postcall response", res);
+              },
+              error => {
+                  console.log("error in response", error);
+              });
+      //}
+     
+    
+    //delete api call
+    //   if(card.isInWishList === true){
+    //       debugger;
+    //    this.DeleteUserWishListUrl = this.DeleteUserWishListUrl + this.emailId + '&listingId='+id;
+    //    this._cservice.observableDeleteHttp(this.DeleteUserWishListUrl, null, false)
+    //       .subscribe((res: Response) => {
+    //       },
+    //       error => {
+    //           console.log("error in response", error);
+    //       });
+    //   }
+       
+  }
+//get wishlist api
+  GetWishList() {
+      // this.GetUserWishList = this.GetUserWishList + this.emailId;
+      console.log("this.GetUserWishList", this.GetUserWishList)
+      this._cservice.observableGetHttp(this.GetUserWishList, null, false)
+          .subscribe((res: Response) => {
+              console.log("this.GetUserWishList Response", res);
+              if (res['length'] != 0) {
+                  this.updateCards(res);
+              }
+          },
+          error => {
+              console.log("error in response", error);
+          });
+  }
+
+  updateCards(wishListData) {
+      if (this.cards && this.cards.length != 0) {
+        //  this.updatedCards = [];
+          for ( let i = 0; i < this.cards.length; i++ ) {
+
+             this.cards[i]['isInWishList'] = wishListData.indexOf(this.cards[i]['_id'] ) > -1
+            //   if ( wishListData.indexOf(this.cards[i]['_id'] ) == -1) {
+            //       this.cards[i]['isInWishList'] = false;
+            //   } else {
+            //       this.cards[i]['isInWishList'] = true;
+            //   }
+
+             // this.updatedCards.push(this.cards[i]);
+          }
+          console.log(this.cards);
+      }
   }
 }
