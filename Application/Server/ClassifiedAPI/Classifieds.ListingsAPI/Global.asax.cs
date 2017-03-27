91c53c271f474d7f4b9eda0b8dd5e7ac40df5858
ashishk-globant <ashish.kulkarni@globant.com>
2017-03-27 14:15:17 +0530

    Changed global.asax for cors

--- Application/Server/ClassifiedAPI/Classifieds.ListingsAPI/Global.asax.cs
+++ Application/Server/ClassifiedAPI/Classifieds.ListingsAPI/Global.asax.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Routing;
 
 namespace Classifieds.ListingsAPI
 {
@@ -16,14 +13,12 @@ namespace Classifieds.ListingsAPI
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            //HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
-            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
             if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
             {
                 HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
                 HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Origin, Authorization, AccessToken, UserEmail");
                 HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Credentials", "true");
                 HttpContext.Current.Response.End();
             }
         }
