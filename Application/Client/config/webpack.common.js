c4b4642a24e695692f3b28be3d422bbcc2e208ff
GLOBANT\bankim.sutaria <bankim.sutaria@globant.com>
2017-04-17 12:49:34 +0530

    Update Application title and tab icons

--- Application/Client/config/webpack.common.js
+++ Application/Client/config/webpack.common.js
@@ -26,7 +26,7 @@ const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
  */
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = {
-  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  title: 'CLASSIFIEDS',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
