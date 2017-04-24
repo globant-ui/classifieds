8827d775ed20091cb2fb8e58b30c78e638626ee2
GLOBANT\bankim.sutaria <bankim.sutaria@globant.com>
2017-04-24 11:17:07 +0530

    Adding missing files

---
+++ Application/Client/src/app/_common/styles/sass/app-custom-variables.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  // Media queries breakpoints
+  // Extra, extra small screen  phone
+  // Extra small screen  phone
+  // Small screen  tablet
+  // Medium screen  desktop
+  // Large screen  wide desktop
+  // Large screen  wide desktop
+  // Extra small screen / phone
+/* Deprecated `$screen-xs` as of v3.0.1
+  // Deprecated `$screen-xs-min` as of v3.2.0
+  // Deprecated `$screen-phone` as of v3.0.1
+  // Small screen / tablet
+/* Deprecated `$screen-sm` as of v3.0.1
+  // Deprecated `$screen-tablet` as of v3.0.1
+  // Medium screen / desktop
+/*  Deprecated `$screen-md` as of v3.0.1
+  // Deprecated `$screen-desktop` as of v3.0.1
+  // Large screen / wide desktop
+/*  Deprecated `$screen-lg` as of v3.0.1
+  // Deprecated `$screen-lg-desktop` as of v3.0.1
+  // So media queries don't overlap when required, provide a maximum
+});
---
+++ Application/Client/src/styles/styles.js
@@ -0,0 +1,6 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  // this file will be extracted to main dist folder and is imported in index.html
+  // This file is for setting global styles
+});
